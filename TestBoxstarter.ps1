39209ece5ced39a01a3a191637e1df605033d9b4
MobilerProblemloser <yuri.tiozzo.asis@gmail.com>
2017-06-29 17:08:35 +0200

    Create TestBoxstarter.ps1

---
+++ TestBoxstarter.ps1
@@ -0,0 +1,13 @@
+Enable-RemoteDesktop
+
+cinst javaruntime -y
+cinst googlechrome -y
+cinst adobereader -y
+cinst adobereader-update -y
+cinst cdburnerxp -y
+cinst 7zip.install -y
+cinst vlc -y
+cinst pdfcreator -y
+cinst silverlight -y
+cinst flashplayerplugin -y
+cinst classic-shell -y
